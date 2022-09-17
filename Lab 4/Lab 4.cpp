3b0557b6d02bdbc86226aca1cc0bd415500fa33b
Pimpa Naree <108038628+Pimpa1@users.noreply.github.com>
2022-09-17 17:23:04 +0700

    Add files via upload

---
+++ Lab 4/Lab 4.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char employee_id[] = "" ;
+	int hour   = 0 ;
+	int ammout = 0 ;
+	
+	while(1){
+		printf ("Input the Employees ID (MAx. 10 chars) : ") ;
+		scanf ("%s",employee_id) ;
+		if ( strlen (employee_id ) <= 10 ) break ;
+	}//end while
+	
+	printf ("Input the working hrs : ") ;
+	scanf ("%d", &hour);
+	printf (" Salary ammount/hr (Bath) : ");
+	scanf ("%d", &ammout ) ;
+	
+	//Output salary
+	printf ("----\n") ;
+	printf ("Eecepted Output : \n") ;
+	printf ("Employees ID = %s\n", employee_id) ;
+	printf ("Ammount/day = %.2f BAth(s)", (float)(ammout * hour)) ;
+	
+	return 0 ;
+}//end function
